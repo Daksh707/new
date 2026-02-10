09b3f2e0b06db96c866f20ba54aa5d12e47d5a90
Daksh707 <152401558+Daksh707@users.noreply.github.com>
2026-02-10 11:43:40 +0530

    Update index.js

--- index.js
+++ index.js
@@ -116,5 +116,8 @@ app.listen(process.env.PORT, () => {
   console.log(`Server running on port ${process.env.PORT}`);
 });
 
+app.get("/", (req, res) => {
+  res.status(200).send("BFHL API is running 🚀");
+});
 
 
